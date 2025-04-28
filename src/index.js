84b4f23bffd327e5d098ad1618d11f75794bdc66
Michelle Hoefer <michellehoefer7@gmail.com>
2025-04-28 15:03:40 +0100

    changed the day ,time & weather functions

--- src/index.js
+++ src/index.js
@@ -2,9 +2,42 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  //let iconElement = document.querySelector("#icon");
+  console.log(response.data);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  //iconElement.innerHTML = response.data.condition.icon;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
